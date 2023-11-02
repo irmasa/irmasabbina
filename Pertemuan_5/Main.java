b6829062bfa6c21487cc8834a8021df2956f086c
NDAA!! <145093531+irmasa@users.noreply.github.com>
2023-11-02 11:51:44 +0700

    Delete Pertemuan_5 directory

--- Pertemuan_5/Main.java
+++
@@ -1,8 +0,0 @@
-package Pertemuan_5;
-public class Main {
-public static void main(String[] args) {
-        mobil_matic matic = new mobil_matic();
-        matic.jalankan();
-    }
-    
-}
--- Pertemuan_5/mobil.java
+++
@@ -1,6 +0,0 @@
-package Pertemuan_5;
-public class mobil {
-public void jalankan(){
-    System.out.println("Masukkan gigi 1, lepas kopling sambil tekan gas");
-}
-}
